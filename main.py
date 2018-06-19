f56948b3e3e9fadd2e4375cdeae37723cde03df2
Jennifer <cutejaneii@hotmail.com>
2018-06-19 10:34:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask,request
 from ptt_crawler import crawl
 import json
@@ -19,6 +20,8 @@ def find_by_keyword():
     return return_data
 
 if __name__ ==  '__main__':
-        app.run(debug=True)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(debug=True, host='0.0.0.0', port=port)
+        #app.run(debug=True)
 
 
