ca2f9b9f659be3c335edf12a9b39d0eb0fb5a9b3
Jennifer <cutejaneii@hotmail.com>
2018-06-20 13:26:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def find_by_keyword():
     return return_data
 
 if __name__ ==  '__main__':
-        port = int(os.environ.get("PORT", 5000))
-        app.run(debug=True, host='0.0.0.0', port=port)
-        #app.run(debug=True)
+        #port = int(os.environ.get("PORT", 5000))
+        #app.run(debug=True, host='0.0.0.0', port=port)
+        app.run(debug=True)
 
 
