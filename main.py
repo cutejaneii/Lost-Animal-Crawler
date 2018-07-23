9dad9e2b5c00c985925e41a7dec9360c4c08fb40
Jennifer <cutejaneii@hotmail.com>
2018-07-24 00:06:06 +0800

    Submit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,request, render_template
-from ptt_crawler import crawl
+from ptt_crawler import ptt_crawl
 import json
 from pyClass import SearchInfo
 app = Flask(__name__)
@@ -22,9 +22,10 @@ def find_by_keyword():
         keyword = request.args.get('keyword')
         findCategory = request.args.get('findCategory')
         pageno = request.args.get('pageno')
-        from_pageno, data = crawl(keyword.encode('utf-8'), findCategory, int(pageno.encode('utf-8')))
+        fromWeb = request.args.get('fromWeb')
+        print(fromWeb)
+        from_pageno, data = ptt_crawl(keyword.encode('utf-8'), findCategory, int(pageno.encode('utf-8')))
         data.sort(key=lambda x: x.post_date, reverse=True)
-        print('finished.')
         
         for i in range(0, len(data)):
             return_data.append({'from_pageno':from_pageno, 'photo_desc': data[i].photo_desc, 'from_web': data[i].from_web, 'article_content': data[i].article_content ,'title':data[i].title, 'url':data[i].url, 'photo_url':data[i].photo_url, 'post_date': str(data[i].post_date)})
@@ -34,7 +35,6 @@ def find_by_keyword():
 
 
 if __name__ ==  '__main__':
-        #app.run(debug=True, port=5000)
         app.run(debug=True)  # Heroku
 
 
